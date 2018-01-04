c802ad232867fb6582db174146b78885e7f3e6f3
Justin Potter <jpotter05@gmail.com>
2018-01-04 16:20:40 -0500

    Got tests passing

--- src/main/scala/Chap1/ShowSyntax.scala
+++ src/main/scala/Chap1/ShowSyntax.scala
@@ -1,10 +1,10 @@
 package Chap1
 
 import cats.Show
+import cats.syntax.show._
 
 object ShowSyntax {
   implicit class ShowOps[A](v: A) {
-    def show(implicit s: Show[A]): String = v.show
     def print(implicit s: Show[A]): Unit = println(v.show)
   }
 }
