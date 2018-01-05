92949ab7ae866279d24740ed499bd8a821c0ba6c
Justin Potter <jpotter05@gmail.com>
2018-01-05 17:34:22 -0500

    Missed a file in last commit

---
+++ src/main/scala/Chap1/EqCatInstances.scala
@@ -0,0 +1,19 @@
+package Chap1
+
+import cats.Eq
+import cats.syntax.eq._
+import cats.instances.int._
+import cats.instances.string._
+
+object EqCatInstances {
+
+  implicit val eqCat =  Eq.instance[Cat] { (c1, c2) =>
+    val eqInt = Eq[Int]
+    val eqStr = Eq[String]
+
+    (c1.name === c2.name) &&
+      (c1.age === c2.age) &&
+      (c1.color === c2.color)
+  }
+
+}
