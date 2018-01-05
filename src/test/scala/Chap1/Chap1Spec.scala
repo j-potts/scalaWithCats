7a7cf25b09289a62d4f2a36307733752e0c5abae
Justin Potter <jpotter05@gmail.com>
2018-01-05 15:50:18 -0500

    Eq excercise

--- src/test/scala/Chap1/Chap1Spec.scala
+++ src/test/scala/Chap1/Chap1Spec.scala
@@ -1,5 +1,6 @@
 package Chap1
 
+import cats.Eq
 import org.scalatest.{FreeSpec, Matchers}
 
 class Chap1Spec extends FreeSpec with Matchers {
@@ -18,6 +19,24 @@ class Chap1Spec extends FreeSpec with Matchers {
     import Chap1.ShowCatInstances._
     "should format Cat output correctly" in {
       Cat("Picard", 45, "tabby").show should be("Picard is 45 year-old tabby cat.")
+    }
+  }
+
+  "Eq" - {
+    import Chap1.EqCatInstances._
+    import cats.instances.option._
+
+    val cat1 = Cat("Garfield",   38, "orange and black")
+    val cat2 = Cat("Heathcliff", 33, "orange and black")
+    "should compare the Cats correctly" in {
+      Eq.eqv(cat1, cat2) should be(false)
+    }
+
+    "should compare the Cats wrapped in the Option type" in {
+      val optionCat1 = Option(cat1)
+      val optionCat2 = Option.empty[Cat]
+
+      Eq.eqv(optionCat1, optionCat2) should be(false)
     }
   }
 
