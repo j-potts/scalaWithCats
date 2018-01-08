2f8f6a5582385813217cec16275f25528ceef372
Justin Potter <jpotter05@gmail.com>
2018-01-08 16:55:07 -0500

    Sum a list of A

--- src/main/scala/Chap2/AdditionMethods.scala
+++ src/main/scala/Chap2/AdditionMethods.scala
@@ -1,13 +1,10 @@
 package Chap2
 
-import cats.instances.int._
 import cats.syntax.semigroup._
 
 object AdditionMethods {
 
-  def add(items: List[Int]): Int =
-    items.fold(Monoid[Int].empty)((acc,i) =>
-      acc |+| i
-    )
+  def add[A: cats.Monoid](items: List[A]): A =
+    items.fold(cats.Monoid[A].empty)(_ |+| _)
 
 }
